4be0c2a1fd6864002193da06e60d521d1a5c2922
dorpivovar <armandooganesyan@gmail.com>
2024-03-05 20:20:35 +0300

    first commit

--- main.py
+++ main.py
@@ -1,36 +1,68 @@
-import sys
-from business import find_business
-from distance import lonlat_distance
-from geocoder import get_coordinates
-from mapapi_PG import show_map
+from flask import Flask, url_for, redirect
+
+app = Flask(__name__)
 
 
-def main():
-    toponym_to_find = " ".join(sys.argv[1:])
-
-    lat, lon = get_coordinates(toponym_to_find)
-    address_ll = f"{lat},{lon}"
-    span = "0.005,0.005"
-
-    organization = find_business(address_ll, span, "аптека")
-    point = organization["geometry"]["coordinates"]
-    org_lat = float(point[0])
-    org_lon = float(point[1])
-    point_param = f"pt={org_lat},{org_lon},pm2dgl"
-
-    points_param = point_param + f"~{address_ll},pm2rdl"
-
-    show_map(map_type="map", add_params=points_param)
-
-    name = organization["properties"]["CompanyMetaData"]["name"]
-    address = organization["properties"]["CompanyMetaData"]["address"]
-    time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
-    distance = round(lonlat_distance((lon, lat), (org_lon, org_lat)))
-
-    snippet = f"Название:\t{name}\nАдрес:\t{address}\nВремя работы:\t{time}\n" \
-              f"Расстояние:\t{distance}м."
-    print(snippet)
+@app.route('/')
+def index():
+    return redirect('/landscapes')
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/landscapes')
+def carousel():
+    return f"""<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous">
+    <link href="{url_for('static', filename='css/style.css')}" rel="stylesheet" type="text/css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    <title>Пейзажи Марса</title>
+</head>
+<body>
+<h1 align="center">Пейзажи Марса</h1>
+
+<div class="container"><div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{url_for('static', filename='img/1.png')}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{url_for('static', filename='img/2.png')}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{url_for('static', filename='img/3.png')}" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div></div>
+</body>
+</html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host="")
